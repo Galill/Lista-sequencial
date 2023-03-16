7840d12adcba8896e88dc7adb34a1233a17a4cc3
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-03-16 19:12:10 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,13 +1,21 @@
 package Lista;
 
 public interface Lista {
-    
+   
+    /*Verifica se a lista está vazia.*/
     public boolean vazia();
+    /*Verifica se a lista está cheia.*/
     public boolean cheia();
+    /*Retorna o tamanho da lista.*/  
     public int tamanho();
+    /*Retorna o elemento dada uma posição na lista.*/  
     public int elemento(int pos);
+    /*Retorna um elemento a partir de um dado fornecido.*/     
     public int posicao(int dado);
+    /*Insere elementos na lista.*/ 
     public boolean inserir(int pos, int dado);
+    /*Remove elementos da lista.*/
     public int remover(int pos);
 
 }
+
